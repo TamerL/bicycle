97bbd4f3378c013eecb26909fd3c3e93ccf2e483
tamerl <eng.tamereid@gmail.com>
2020-04-05 17:43:30 +1000

    first commit

---
+++ gear.rb
@@ -0,0 +1,29 @@
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @wheel= Wheel.new(rim, tire)
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
+  end
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+puts Gear.new(52, 11, 24, 1.25).wheel.diameter
+puts Gear.new(52, 11, 24, 1.25).wheel.rim
+puts Gear.new(52, 11, 24, 1.25).wheel.tire
