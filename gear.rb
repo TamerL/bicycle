70b0ff1cb001e7f98c10c21effc06a5aa57130c1
tamerl <eng.tamereid@gmail.com>
2020-04-05 18:01:43 +1000

    achieving SRP, splitting wheel from gear

--- gear.rb
+++ gear.rb
@@ -1,29 +1,24 @@
 class Gear
+
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, rim, tire)
+
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog = cog
-    @wheel= Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
     chainring / cog.to_f
   end
 
-
   def gear_inches
     ratio * wheel.diameter
   end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
-  end
 end
-
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
-puts Gear.new(52, 11, 24, 1.25).wheel.diameter
-puts Gear.new(52, 11, 24, 1.25).wheel.rim
-puts Gear.new(52, 11, 24, 1.25).wheel.tire
+#
+# puts Gear.new(52, 11, 26, 1.5).gear_inches
+# puts Gear.new(52, 11, 24, 1.25).gear_inches
+# puts Gear.new(52, 11, 24, 1.25).wheel.diameter
+# puts Gear.new(52, 11, 24, 1.25).wheel.rim
+# puts Gear.new(52, 11, 24, 1.25).wheel.tire
